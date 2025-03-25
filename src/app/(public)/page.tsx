6736244f25a917a7577d0d5d5e14812723a59ed9
Caio Vellani <caiovellani@outlook.com>
2025-03-25 20:42:20 -0300

    Add Hero component to home page
    - Import Hero component from public components
    - Wrap content in flex column container with min-height screen
    - Insert Hero component below Header in the page structure

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,9 +1,14 @@
 import { Header } from '@/app/(public)/components/header'
+import { Hero } from '@/app/(public)/components/hero'
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
+
+      <div>
+        <Hero />
+      </div>
     </div>
   )
 }
