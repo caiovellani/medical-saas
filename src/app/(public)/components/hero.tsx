5da0570b2449b22b9856b15481eb4d0acb16cbf2
Caio Vellani <caiovellani@outlook.com>
2025-03-26 15:20:19 -0300

    feat: update padding bottom from container

--- src/app/(public)/components/hero.tsx
+++ src/app/(public)/components/hero.tsx
@@ -5,7 +5,7 @@ import doctorImg from '@/../public/doctor-hero.png'
 export function Hero() {
   return (
     <section className="bg-white">
-      <div className="container mx-auto px-4 pt-20 sm:px-6 lg:px8">
+      <div className="container mx-auto pb-4 sm:pb-0 px-4 pt-20 sm:px-6 lg:px8">
         <main className="flex items-center justify-center">
           <article className="space-y-8 flex-[2] max-w-3xl flex flex-col justify-center">
             <h1 className="text-4xl font-bold lg:text-5xl max-w-2xl tracking-tight">
