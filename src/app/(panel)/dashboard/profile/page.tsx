83b52d9eebda13b767e826f7f8edb2ca46cf104a
Caio Vellani <caiovellani@outlook.com>
2025-04-01 22:07:56 -0300

    feat(profile): Add authentication and user data fetching to profile page
    - Implementing session validation to ensure only authenticated users can access the profile page
    - Adding user data fetching functionality via the getUserData function
    - Adding redirects to the homepage for unauthenticated users or when user data cannot be found
    - Converting the Profile component to an async function to support server-side data fetching

--- src/app/(panel)/dashboard/profile/page.tsx
+++ src/app/(panel)/dashboard/profile/page.tsx
@@ -1,4 +1,21 @@
-export default function Profile() {
+import { getUserData } from '@/app/(panel)/dashboard/profile/data-access/get-info-user'
+import getSession from '@/lib/get-session'
+import { redirect } from 'next/navigation'
+
+export default async function Profile() {
+  const session = await getSession()
+
+  if (!session) {
+    redirect('/')
+  }
+
+  const user = await getUserData({ userId: session.user?.id })
+  console.log('getUserData: ', user)
+
+  if (!user) {
+    redirect('/')
+  }
+
   return (
     <section>
       <h1>Página Perfil</h1>
